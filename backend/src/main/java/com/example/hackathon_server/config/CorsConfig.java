42063672ee892d462e4fb3b5fc70a2db3edffe05
Lse-Yeong <ocdee39@gmail.com>
2024-11-09 12:08:14 +0900

    cors 코드 수정

--- backend/src/main/java/com/example/hackathon_server/config/CorsConfig.java
+++ backend/src/main/java/com/example/hackathon_server/config/CorsConfig.java
@@ -1,20 +1,28 @@
 package com.example.hackathon_server.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+
     }
+
 }
 
